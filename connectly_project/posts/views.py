cc3aea39b0c2139aef5c05a5380de319b50f3a62
prilayesa <lr.ajabejo@mmdc.mcl.edu.ph>
2026-01-17 06:50:07 +0800

    chore: finish Week 2 Activities

---
+++ connectly_project/posts/views.py
@@ -0,0 +1,52 @@
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import Post
+from .models import User
+
+# Function 1: To get users
+def get_users(request):
+    try:
+        users = list(User.objects.values('id', 'username', 'email', 'created_at'))
+        return JsonResponse(users, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+
+# Function 2: To create a new user
+@csrf_exempt
+def create_user(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            user = User.objects.create(username=data['username'], email=data['email'])
+            return JsonResponse({'id': user.id, 'message': 'User created successfully'}, status=201)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
+
+# Function 3: Retrieve All Posts
+def get_posts(request):
+    try:
+        posts = list(Post.objects.values('id', 'content', 'author', 'created_at'))
+        return JsonResponse(posts, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+#  Function 4:  To Create a Post
+@csrf_exempt
+def create_post(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            author = User.objects.get(id=data['author'])
+            post = Post.objects.create(content=data['content'], author=author)
+            return JsonResponse({'id': post.id, 'message': 'Post created successfully'}, status=201)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'Author not found'}, status=404)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
+
+
+
